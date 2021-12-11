7d4a1b992c9f0bbf38d484a2f728f86ab09087ba
Tom <tomkasull@gmail.com>
2021-12-10 23:17:41 -0600

    os abstraction, tensorflowtts tacotron + melgan wav generation on ubuntu

--- README.md
+++ README.md
@@ -3,9 +3,12 @@ Basic socket chat server utilizing aiohttp &amp; socketio.
 
 # Installation
 ```
-# install dependencies
-pip install -r requirements.txt
+# install dependencies if on windows
+pip install -r windows.txt
 
-# run server
+#install dependencies if on ubuntu
+pip install -r ubuntu.txt
+
+# run server, tensorflowtts will be disabled on windows
 python server.py
 ```
--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 import asyncio
-
 from aiohttp import web
-
 import socketio
 import base64
+import os
+
+if os.name != 'nt': #tensorflowtts only works on ubuntu
+    import soundfile as sf
+    import numpy as np
+    import tensorflow as tf
+    from tensorflow_tts.inference import AutoProcessor
+    from tensorflow_tts.inference import TFAutoModel
+    processor = AutoProcessor.from_pretrained("tensorspeech/tts-tacotron2-ljspeech-en")
+    tacotron2 = TFAutoModel.from_pretrained("tensorspeech/tts-tacotron2-ljspeech-en")
+    melgan = TFAutoModel.from_pretrained("tensorspeech/tts-melgan-ljspeech-en")
 
 sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
@@ -21,8 +30,19 @@ async def chatMessage(sid, msg):
 
 
 @sio.event
-async def wavMessage(sid, msg):
-    #todo generate tts wav based on msg
+async def wavMessage(sid, msg):    
+    if os.name != 'nt': #tensorflowtts only works on ubuntu
+        input_ids = processor.text_to_sequence(msg)
+        #tacotron2 inference (text-to-mel)
+        decoder_output, mel_outputs, stop_token_prediction, alignment_history = tacotron2.inference(
+            input_ids=tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
+            input_lengths=tf.convert_to_tensor([len(input_ids)], tf.int32),
+            speaker_ids=tf.convert_to_tensor([0], dtype=tf.int32),
+        )
+        #melgan inference (mel-to-wav)
+        audio = melgan.inference(mel_outputs)[0, :, 0]
+        #create file from melgan wav data & send it off
+        sf.write('./ljs.wav', audio, 22050, "PCM_16")
     encode_string = base64.b64encode(open("ljs.wav", "rb").read()).decode()
     await sio.emit('wavMessage', encode_string, room=sid)
 
