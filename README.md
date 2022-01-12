d4a3dd265ca7bb0b341984b2923ccc0903f99385
Tom <tomkasull@gmail.com>
2022-01-12 15:07:27 -0600

    dirty interface for tts & blenderbot, basic stt inference

--- README.md
+++ README.md
@@ -12,3 +12,20 @@ pip install -r ubuntu.txt
 # run server, tensorflowtts will be disabled on windows
 python server.py
 ```
+
+# Virtual Env Installation
+```
+# create a virtual env for the project using your desired python version
+C:\Users\username\AppData\Local\Programs\Python\Python310\python.exe -m venv venv
+
+# activate the newly created virtual environment & install dependencies (on windows)
+./venv/scripts/activate.ps1
+pip install -r windows.txt
+
+# activate the newly created virtual environment & install dependencies (on ubuntu)
+./venv/scripts/activate
+pip install -r ubuntu.txt
+
+# run the server
+python server.py
+```
--- server.py
+++ server.py
@@ -1,50 +1,94 @@
-import asyncio
+import requests
+from asyncio.windows_events import NULL
 from aiohttp import web
 import socketio
 import base64
 import os
-
-if os.name != 'nt': #tensorflowtts only works on ubuntu
-    import soundfile as sf
-    import numpy as np
-    import tensorflow as tf
-    from tensorflow_tts.inference import AutoProcessor
-    from tensorflow_tts.inference import TFAutoModel
-    processor = AutoProcessor.from_pretrained("tensorspeech/tts-tacotron2-ljspeech-en")
-    tacotron2 = TFAutoModel.from_pretrained("tensorspeech/tts-tacotron2-ljspeech-en")
-    melgan = TFAutoModel.from_pretrained("tensorspeech/tts-melgan-ljspeech-en")
+import json
+from websocket import create_connection
+import wave
+from timeit import default_timer as timer
+import numpy as np
+from stt import Model
 
 sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
 sio.attach(app)
 
+# load speech to text model
+scorer = "coqui-stt-0.9.3-models.scorer"
+model = "model.tflite"
+print("Loading model from file {}".format(model))
+model_load_start = timer()
+ds = Model(model)
+model_load_end = timer() - model_load_start
+print("Loaded model in {:.3}s.".format(model_load_end))
+
+# load speech to text scorer
+print("Loading scorer from files {}".format(scorer))
+scorer_load_start = timer()
+ds.enableExternalScorer(scorer)
+scorer_load_end = timer() - scorer_load_start
+print("Loaded scorer in {:.3}s.".format(scorer_load_end))
+
+# serve html
 async def index(request):
     with open('app.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
 
+ws = NULL
+try:    
+    ws = create_connection("ws://localhost:8080/websocket") #dirty, should close at some point
+    ws.send(json.dumps({"text": "begin"})) #sequence to initiate basic parl-ai socket instance
+    result =  ws.recv()                    ##################################################
+    ws.send(json.dumps({"text": "begin"})) ##################################################
+    result = ws.recv()                     ##################################################
+    print("Received '%s'" % result)        ##################################################
+except:
+    print("Unable to connect to BlenderBot.")
 
 @sio.event
 async def chatMessage(sid, msg):
     print(f'chatMessage received from: {sid}')
-    await sio.emit('chatMessage', msg)
+    await sio.emit('chatMessage', {"User": msg})
+    ws.send(json.dumps({"text": f"{msg}"}))
+    result = ws.recv()
+    print("Received '%s'" % result)
+    bbResponse = json.loads(result)
+    await sio.emit('chatMessage', {"BlenderBot": bbResponse["text"]})
 
 
 @sio.event
 async def emitText(sid, msg):    
-    if os.name != 'nt': #tensorflowtts only works on ubuntu
-        input_ids = processor.text_to_sequence(msg)
-        #tacotron2 inference (text-to-mel)
-        decoder_output, mel_outputs, stop_token_prediction, alignment_history = tacotron2.inference(
-            input_ids=tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
-            input_lengths=tf.convert_to_tensor([len(input_ids)], tf.int32),
-            speaker_ids=tf.convert_to_tensor([0], dtype=tf.int32),
-        )
-        #melgan inference (mel-to-wav)
-        audio = melgan.inference(mel_outputs)[0, :, 0]
-        #create file from melgan wav data & send it off
-        sf.write('./ljs.wav', audio, 22050, "PCM_16")
+    ws.send(json.dumps({"text": f"{msg}"}))
+    result = ws.recv()
+    print("Received '%s'" % result)
+    bbResponse = json.loads(result)
+    params = {
+        'text': {bbResponse["text"]},
+        'speaker_id': 'p243',
+        'style_wav': ''
+    }
+    r = requests.get("http://localhost:5002/api/tts", params=params)
+    open('ljs.wav', 'wb').write(r.content)
     encode_string = base64.b64encode(open("ljs.wav", "rb").read()).decode()
     await sio.emit('avatarResponse', {'text': msg, 'wav': encode_string}, room=sid)
+
+
+@sio.event
+async def emitAudio(sid, msg):
+    decoded_data = base64.b64decode(msg)
+    with wave.open("out.wav", 'wb') as wav:
+        wav.setparams((1, 2, 16000, 0, 'NONE', 'NONE'))
+        wav.writeframes(decoded_data)
+        fs_orig = wav.getframerate()
+        audio = np.frombuffer(wav.readframes(wav.getnframes()), np.int16)
+        audio_length = wav.getnframes() * (1 / fs_orig)
+        inference_start = timer()
+        out = ds.stt(audio)
+        inference_end = timer() - inference_start
+        print("Inference took %0.3fs for %0.3fs audio file." % (inference_end, audio_length))
+        await sio.emit('chatMessage', {"User": out})
 
 
 @sio.event
@@ -60,4 +104,4 @@ def disconnect(sid):
 
 app.router.add_get('/', index)
 if __name__ == '__main__':
-    web.run_app(app)
+    web.run_app(app, port=1337)
